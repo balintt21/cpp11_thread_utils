5c8ea1f13651b179c07f58de84f882561dba0759
Balint Telkes <balintt21@gmail.com>
2018-10-11 09:22:49 +0200

    Create worker_thread.h
    new file

---
+++ src/worker_thread.h
@@ -0,0 +1,47 @@
+#ifndef _WORKER_THREAD_H_
+#define _WORKER_THREAD_H_
+
+#include <atomic>
+
+#include "thread.h"
+
+namespace thread_utils
+{
+    class WorkerThread
+    {
+    private:
+        std::atomic_bool    mIsRunning;
+        Thread              mThread;
+    public:
+        WorkerThread(const std::string& name) : mIsRunning(false), mThread(name) {}
+
+        inline bool start(const std::function<bool ()>& loop_function)
+        {
+            mIsRunning.store(true);
+            return mThread.run([loop_function, this]()
+            {
+                while(mIsRunning.load())
+                {
+                    if( !loop_function() ) 
+                    {
+                        mIsRunning.store(false); 
+                        break; 
+                    }
+                }
+            });
+        }
+
+        inline void stop(bool wait = false)
+        {
+            mIsRunning.store(false);
+            if( wait ) 
+            { mThread.join(); }
+        }
+
+        inline bool isRunning() const { return mIsRunning.load(); }
+
+        inline Thread& thread() { return mThread; }
+    };
+}
+
+#endif
